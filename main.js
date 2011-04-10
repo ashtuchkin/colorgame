b2df659e2a98f0b912f68d8b90dc574482bee9ba
ashtuchkin <ashtuchkin@gmail.com>
2011-04-10 14:05:16 +0000

    Test commit from Cloud9

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express.createServer();
 
 app.get('/', function(req, res){
-    res.send('Hello World');
+    res.send('Hello World!');
 });
 
 app.listen(process.env.C9_PORT);
